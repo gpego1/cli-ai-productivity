fab88391660cd65ace3895335d3c43d48a6b855a
gpego1 <gpego786@gmail.com>
2026-06-27 10:22:21 -0300

    refactor(utils): read_file now makes a validation if the file really exists

--- utils/file_reader.py
+++ utils/file_reader.py
@@ -3,27 +3,33 @@ import pdfplumber
 from pathlib import Path
 
 def read_file(file_path : str | Path) -> str | list[dict]:
-    file_path = Path(file_path)
+    try:
+        file_path = Path(file_path)
 
-    if not file_path.exists():
-        raise FileNotFoundError(f"File Not Found: ${file_path}")
-    
-    suffix = file_path.suffix.lower()
-
-    if suffix in (".txt", ".md"):
-        return file_path.read_text(encoding="utf-8")
-    
-    if suffix == ".csv":
-        with file_path.open(encoding = "utf-8", newline="") as f:
-            reader = csv.DictReader(f)
-            return list(reader)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File Not Found: ${file_path}")
         
-    if suffix == ".pdf":
-        pages: list[str] = []
-        with pdfplumber.open(file_path) as pdf:
-            for page in pdf.pages:
-                text = page.extract_text() or ""
-                pages.append(text)
-        return "\n\n".join(pages)
+        suffix = file_path.suffix.lower()
+
+        if suffix in (".txt", ".md"):
+            return file_path.read_text(encoding="utf-8")
+        
+        if suffix == ".csv":
+            with file_path.open(encoding = "utf-8", newline="") as f:
+                reader = csv.DictReader(f)
+                return list(reader)
+            
+        if suffix == ".pdf":
+            pages: list[str] = []
+            with pdfplumber.open(file_path) as pdf:
+                for page in pdf.pages:
+                    text = page.extract_text() or ""
+                    pages.append(text)
+            return "\n\n".join(pages)
+        
+        raise ValueError(f"File extension could not be loaded '{suffix}'. Use .txt, .md, .csv or .pdf")
     
-    raise ValueError(f"File extension could not be loaded '{suffix}'. Use .txt, .md, .csv or .pdf")
+    except FileNotFoundError:
+        print(f"Could not find file: {file_path}")
+    
+   
