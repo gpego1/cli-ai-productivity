94603f9cbcfd9ebd23de5293c607631c874355ef
gpego1 <gpego786@gmail.com>
2026-07-19 14:54:33 -0300

    update: switched llm methods to use groq instead of openai lib

--- utils/call_llm.py
+++ utils/call_llm.py
@@ -1,21 +1,31 @@
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
+from groq import Groq
 
 load_dotenv()
 model = os.getenv("MODEL")
 api_key = os.getenv("OPENAI_API_KEY")
 base_url = os.getenv("BASE_URL")
 
-client = OpenAI(
-    base_url = base_url, 
-    api_key = api_key
-)
+client = Groq(api_key=api_key)
+
 
 def call_llm(prompt: str) -> str:
     response = client.chat.completions.create(
         model = model,
         messages = [{"role" : "user", "content": prompt}],
-        temperature = 0.7
+        temperature = 0.7,
+        max_completion_tokens=2048,
+        top_p=1,
+        stream=True,
+        stop=None
     )
-    return response.choices[0].message.content
+    full_text = ""
+
+    for chunk in response:
+        content = chunk.choices[0].delta.content
+        if content:
+            full_text += content
+        
+    return full_text
+    
